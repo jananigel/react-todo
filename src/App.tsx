19b84f8b25fd49b993248738e454dfcb6b719319
kyle <kyle.chang@ackuretta.com>
2024-09-23 19:32:27 +0800

    feat: import the TodoProvider for Todo form

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import './App.css'
+import './App.css';
 import TodoForm from './components/todo-form/todo-form';
 import TodoList from './components/todo-list/todo-list';
+import { TodoProvider } from './contexts/todo-context';
 
 function App() {
-
   return (
     <>
-      <TodoForm />
-    <TodoList />
+      <TodoProvider>
+        <TodoForm />
+        <TodoList />
+      </TodoProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
